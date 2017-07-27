31a1999a33e20bada8d91573b1716a1fc360e56c
Atsuhiro Hiramoto <gibakusin1999@gmail.com>
2017-07-28 00:02:36 +0900

    Add files via upload

---
+++ Basic_Programming/ST/三択問題.js
@@ -0,0 +1,15 @@
+function test(doko) {
+	var errors = "“š‚¦‚ğ‘I‘ğ‚µ‚Ä‰º‚³‚¢B";
+	var dore = -1;
+	for (var i = 0; i < 3; i++) {
+		if (document.forms[doko].elements[i].checked === true) {
+			dore = document.forms[doko].elements[i].value;
+		}
+	}
+		if (dore === -1) {
+			alert(errors);
+		} else {
+			alert(dore);
+		}
+	
+}
